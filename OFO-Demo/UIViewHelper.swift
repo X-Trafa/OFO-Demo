68b308aa75dd3a45d584408035b08f0d490edfa6
X-Trafa <imtrafa@163.com>
2020-02-23 17:26:04 +0800

    Init

---
+++ OFO-Demo/UIViewHelper.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewHelper.swift
+//  OFO-Demo
+//
+//  Created by Trafa on 2020/2/23.
+//  Copyright © 2020 Trafa. All rights reserved.
+//
+
+import Foundation
---
+++ OFO-Demo/UnlockViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UnlockViewController.swift
+//  OFO-Demo
+//
+//  Created by Trafa on 2020/2/23.
+//  Copyright © 2020 Trafa. All rights reserved.
+//
+
+import UIKit
+
+class UnlockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
